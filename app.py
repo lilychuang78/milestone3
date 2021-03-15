cc4e5beb5f5bc53745aa95c64b0652926b16619e
lilychuang78 <lilychuang78@gmail.com>
2021-03-15 16:09:16 +0000

    add login template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash,check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -25,6 +26,9 @@ def get_intents():
     intents = mongo.db.intents.find()
     return render_template("intents.html", intents=intents)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
