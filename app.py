13bc7ea548b29548f3ded29d2d32b90bebadc554
lilychuang78 <lilychuang78@gmail.com>
2021-03-15 19:30:35 +0000

    fix log in flash messages

--- app.py
+++ app.py
@@ -31,19 +31,46 @@ def login():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
+
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Hello, {} again".format(
-                    request.form.get("username")))
-                return redirect(url_for("search", username=session["user"]))
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hello, {} again".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "home", username=session["user"]))
+            else:
+                flash("wrong username or password")
+                return redirect(url_for("login"))
+
         else:
             flash("wrong username or password")
             return redirect(url_for("login"))
-    else:
-        flash("please register")
+
     return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("already registered")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("you are now registered")
+        return redirect(url_for("home", username=session["user"]))
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
