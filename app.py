c3735b6a708bfd3d4431b964f952da948ace9ff1
lilychuang78 <lilychuang78@gmail.com>
2021-03-15 16:23:50 +0000

    add login function

--- app.py
+++ app.py
@@ -28,6 +28,21 @@ def get_intents():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Hello, {} again".format(
+                    request.form.get("username")))
+                return redirect(url_for("search", username=session["user"]))
+        else:
+            flash("wrong username or password")
+            return redirect(url_for("login"))
+    else:
+        flash("please register")
     return render_template("login.html")
 
 
