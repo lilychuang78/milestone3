77962f63d0b02f1ea2dee07ec7e01827f5b36476
lilychuang78 <lilychuang78@gmail.com>
2021-03-16 08:49:09 +0000

    add logout function

--- app.py
+++ app.py
@@ -73,6 +73,12 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/logout")
+def logout():
+    flash("logged out")
+    session.pop("user")
+    return redirect(url_for("home"))
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
