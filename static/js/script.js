1dae2f54a32780adfeed8cd73d2909736806478d
lilychuang78 <lilychuang78@gmail.com>
2021-03-17 15:08:37 +0000

    add popup window before deletion

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     $(".sidenav").sidenav(); /*jquery sidenav*/
     $("#flash").fadeOut(3000); /*flash message fade out*/
     $('.collapsible').collapsible(); /*collasible*/
+    $('.modal').modal({preventScrolling: false}); /*confirmation before deleting*/
 });
